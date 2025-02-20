211abf2a39fd486688f791bd0d8a5e3a50175be1
Plamen <pslavchev1102@gmail.com>
2025-02-20 22:09:14 +0200

    Adding Spring project

---
+++ src/main/java/com/rungroup/web/service/impl/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.rungroup.web.service.impl;
+
+import com.rungroup.web.dto.RegistrationDto;
+import com.rungroup.web.models.Role;
+import com.rungroup.web.models.UserEntity;
+import com.rungroup.web.repository.RoleRepository;
+import com.rungroup.web.repository.UserRepository;
+import com.rungroup.web.service.UserService;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void saveUser(RegistrationDto registrationDto) {
+        UserEntity user = new UserEntity();
+        user.setUsername(registrationDto.getUsername());
+        user.setEmail(registrationDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
+        Role role = roleRepository.findByName("USER");
+        user.setRoles(Arrays.asList(role));
+        userRepository.save(user);
+    }
+
+    @Override
+    public UserEntity findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public UserEntity findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
